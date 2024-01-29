f13cda42a3896ad694c6e4ccde125942b95dbbec
a-reznikov <a.reznnikov@gmail.com>
2024-01-29 13:18:32 +0300

    feat: implement solutions for wt

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,33 @@
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { cpus } from 'os';
+import { Worker, workerData } from 'node:worker_threads';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const pathToFile = resolve(__dirname, 'worker.js');
+const processes = cpus();
+let incrementalNumber = 10;
+
+
 const performCalculations = async () => {
-    // Write your code here
+    const results = await Promise.allSettled(processes.map(() => {
+        return new Promise((res, rej) => {
+            const worker = new Worker(pathToFile, {
+                workerData: incrementalNumber++,
+            })
+
+            worker.on('message', (result) => res(result));
+            worker.on('error', (result) => rej(result));
+        })
+    }))
+
+    const statuses = results.map(({ status, value }) => {
+        const isSuccess = status === 'fulfilled' ? true : false;
+
+        return { status: isSuccess ? "resolved" : 'error', value: isSuccess ? value : null };
+    })
+
+    console.log(statuses);
 };
 
 await performCalculations();
