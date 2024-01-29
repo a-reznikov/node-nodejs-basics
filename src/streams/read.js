7768c286e047ae14126b899c1d0315fd45594f3c
a-reznikov <a.reznnikov@gmail.com>
2024-01-29 11:32:01 +0300

    feat: implement solutions for streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,16 @@
+import { getPathToFile } from '../utils/getPathToFile.js';
+import { createReadStream } from 'node:fs';
+import { pipeline } from 'node:stream/promises';
+
+const pathToFile = getPathToFile(import.meta.url, 'fileToRead.txt')
+
 const read = async () => {
-    // Write your code here 
+    try {
+        const stream = createReadStream(pathToFile, 'utf-8');
+        await pipeline(stream, process.stdout);
+    } catch (error) {
+        throw error;
+    }
 };
 
 await read();
